c922ad13059d5c3519e195f1f37d334b92225ed2
WillDRuss <willrussell90@hotmail.com>
2019-04-05 17:43:43 +0100

    adding updates to show move history and button to step back through moves

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function ResetButton(props) {
 	return (
-		<button onClick={props.onReset}>
+		<button onClick={props.onReset} id="reset">
 			{'Restart Game'}
 		</button>
 	);
@@ -23,62 +23,24 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
-
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		if (calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			squares: squares,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
-
-	handleReset() {
-		this.setState({
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		})
-	}
-
   renderSquare(i) {
     return (
     	<Square 
-    		value={this.state.squares[i]}
-    		onClick={() => this.handleClick(i)} 
+    		value={this.props.squares[i]}
+    		onClick={() => this.props.onClick(i)} 
     	/>
     );
   }
 
   renderResetButton() {
   	return (
-  		<ResetButton onReset={() => this.handleReset()}/>
+  		<ResetButton onReset={() => this.props.onReset()}/>
   	);
   }
 
   render() {
-  	const winner = calculateWinner(this.state.squares);
-  	let status;
-  	if (winner === 'X' || winner === 'O') {
-  		status = 'Winner: ' + winner;
-  	} else if (winner === 'tie') {
-  		status = 'Tied game!'
-  	} else {
-  		status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  	} 
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -103,15 +65,89 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state =  {
+			history: [{
+				squares: Array(9).fill(null),
+			}],
+			stepNumber: 0,
+			xIsNext: true,
+		}
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length -1];
+		const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			history: history.concat([{
+				squares: squares,
+			}]),
+			stepNumber: history.length,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	handleReset() {
+		this.setState({
+			history: [{
+				squares: Array(9).fill(null),
+			}],
+			stepNumber: 0,
+			xIsNext: true,
+		})
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
+		});
+	}
+
   render() {
+  	const history = this.state.history;
+  	const current = history[this.state.stepNumber];
+  	const winner = calculateWinner(current.squares);
+
+  	const moves = history.map((step, move) => {
+  		if (move > 0) {
+  			return (
+	  			<li key={move}>
+	  				<button onClick={() => this.jumpTo(move)}>
+	  					{'Return to move #' + move }
+	  				</button>
+	  			</li>
+	  		);
+  		}
+  	});
+
+  	let status;
+  	if (winner === 'X' || winner === 'O') {
+  		status = 'Winner: ' + winner;
+  	} else if (winner === 'tie') {
+  		status = 'Tied game!'
+  	} else {
+  		status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+  	} 
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+          	squares={current.squares}
+          	onClick={(i) => this.handleClick(i)}
+          	onReset={() => this.handleReset()}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -133,9 +169,9 @@ function calculateWinner(squares) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
-    } else if (squares.filter(elem => elem === null).length === 0) {
+    } 
+  } if (squares.filter(elem => elem === null).length === 0) {
     	return 'tie';
-    }
   }
   return null;
 }
